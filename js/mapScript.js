6f81d93e7c407f6bd4ef1ff65084dd20137f902f
Thanh-81 <chiemthanhhuynh@gmail.com>
2024-05-12 20:33:51 +0200

    pffff
    een aantal fouten eruit gehaald en verder hieraan geprutst

---
+++ js/mapScript.js
@@ -0,0 +1,11 @@
+
+let map = L.map('apMap').setView([51.23009, 4.41616], 17);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+let polygon = L.rectangle([[51.23041, 4.4155], [51.22991, 4.41675]], { color: 'red', fillColor: 'red', fillOpacity: 0.5 }).addTo(map);
+
+let marker = L.marker([51.23009, 4.41616]).addTo(map); marker.bindPopup("<b>AP-Hogeschool</b><br>Ellermanstraat 33").openPopup();
